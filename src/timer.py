81197146437c4126f024e5c1fa2ffbcc97aa8af0
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2020-12-09 11:05:27 -0500

    relocated stuff to src/ removed debug prints started working on "refresh"

--- src/timer.py
+++ src/timer.py
@@ -89,18 +89,21 @@ def update_text():
         obs.obs_source_update(source, settings)
 
 def refresh_pressed(props, prop):
-    # TODO update list of races
-    #update_text()
+    # TODO update list of races and sources
     None
 
-
+def callback(props, prop, *args, **kwargs):
+    p = obs.obs_properties_get(props, "button")
+    obs.obs_property_set_description(p, f"button pressed")
+    p = obs.obs_properties_get(props, "source")
+    fill_source_list(p)
 
 # ------------------------------------------------------------
 
 def script_description():
-    return "Select a text source to use as your timer and enter your full" + \
-    "username on racetime.gg  (including discriminator). This only needs" + \
-    "to be done once.\n\nThen select the race room each race you join and" + \
+    return "Select a text source to use as your timer and enter your full " + \
+    "username on racetime.gg  (including discriminator). This only needs " + \
+    "to be done once.\n\nThen select the race room each race you join and " + \
     "stop worrying about whether you started your timer or not."
 
 def script_update(settings):
@@ -119,7 +122,6 @@ def script_update(settings):
     r = None
     if (race != obs.obs_data_get_string(settings, "race")):
         race = obs.obs_data_get_string(settings, "race")
-        print(f"race is {race}\n")
         r = racetime_client.get_race(race)
 
     if r is not None and source_name != "":
@@ -129,18 +131,21 @@ def script_update(settings):
 #def script_defaults(settings):
 #	obs.obs_data_set_default_int(settings, "interval", 30)
 
-def script_properties():
-    props = obs.obs_properties_create()
-
-    p = obs.obs_properties_add_list(props, "source", "Text Source", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
+def fill_source_list(p):
+    obs.obs_property_list_clear(p)
     with source_list_ar() as sources:
         if sources is not None:
             for source in sources:
                 source_id = obs.obs_source_get_unversioned_id(source)
                 if source_id == "text_gdiplus" or source_id == "text_ft2_source":
                     name = obs.obs_source_get_name(source)
-                    print(f"{name}\n")
                     obs.obs_property_list_add_string(p, name, name)
+
+def script_properties():
+    props = obs.obs_properties_create()
+
+    p = obs.obs_properties_add_list(props, "source", "Text Source", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
+    fill_source_list(p)
 
     obs.obs_properties_add_text(props, "username", "Username", obs.OBS_TEXT_DEFAULT)
 
@@ -152,5 +157,6 @@ def script_properties():
 
     p = obs.obs_properties_add_list(props, "race", "Race", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
 
-    obs.obs_properties_add_button(props, "button", "Refresh", refresh_pressed)
+    b = obs.obs_properties_add_button(props, "button", "Refresh", refresh_pressed)
+    obs.obs_property_set_modified_callback(b, callback)
     return props
