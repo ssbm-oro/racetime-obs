0ba39a87b73afa51fa977f9a04c7fc75bedf57f1
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2020-12-10 21:25:50 -0500

    option to change timer color based on finish place

--- src/timer.py
+++ src/timer.py
@@ -81,20 +81,39 @@ entrant_status_value    = ""
 check_race_updates      = False
 close_thread            = False
 start_delay             = None
+entrant_place           = None
+use_podium_colors       = False
+racing_color            = 0xFFFFFF
+first_color             = 0xFF0000
+second_color            = 0x00FF00
+third_color             = 0x0000FF
+finished_color          = 0xFFFFFF
 
 # ------------------------------------------------------------
 
 def update_text():
     """takes scripted_text , sets its value in obs  """
 
+    color = None
     if race_status_value == "open" or race_status_value == "invitational":
         time = "-" + str(start_delay) + ".0"
     elif finish_time is not None:
         time = str(finish_time)[:9]
+        if use_podium_colors:
+            if entrant_place == 1:
+                color = first_color
+            elif entrant_place == 2:
+                color = second_color
+            elif entrant_place == 3:
+                color = third_color
+            else:
+                color = finished_color
     elif entrant_status_value == "dnf" or entrant_status_value == "dq" \
     or race_status_value == "cancelled":
         time = "--:--:--.-"
     elif started_at is not None:
+        if use_podium_colors:
+            color = racing_color
         timer = datetime.now(timezone.utc) - started_at
         if race_status_value == "pending":
             time = "-0:00:{:2.1f}".format(timer.total_seconds() * -1.0)
@@ -104,7 +123,10 @@ def update_text():
         return
     with source_ar(source_name) as source, data_ar() as settings:
         obs.obs_data_set_string(settings, "text", time)
+        if use_podium_colors:
+            set_color(source, settings, color)
         obs.obs_source_update(source, settings)
+        
 
 def race_updater():
     while True:
@@ -125,6 +147,7 @@ def update_race(r: Race):
     global entrant_status_value
     global started_at
     global start_delay
+    global entrant_place
 
     started_at = r.started_at
     start_delay = r.start_delay
@@ -133,6 +156,7 @@ def update_race(r: Race):
     if entrant is not None:
         if entrant.finish_time:
             finish_time = entrant.finish_time
+            entrant_place = entrant.place
         entrant_status_value = entrant.status.value
 
 
@@ -153,6 +177,25 @@ def new_race_selected(props, prop, settings):
         obs.obs_data_set_default_string(settings, "race_info", "Race not found")
     return True
 
+def podium_toggled(props, prop, settings):
+    vis = obs.obs_data_get_bool(settings, "use_podium")
+    obs.obs_property_set_visible(obs.obs_properties_get(props, "podium_group"), vis)
+    return True
+
+# copied and modified from scripted-text.py by UpgradeQ
+def set_color(source, settings, color):
+    if color is None:
+        return
+    source_id = obs.obs_source_get_unversioned_id(source)
+    if source_id == "text_gdiplus":
+        obs.obs_data_set_int(settings, "color", color)  # colored text
+
+    else:  # freetype2,if taken from user input it should be reversed for getting correct color
+        number = "".join(hex(color)[2:])
+        color = int("0xff" f"{number}", base=16)
+        obs.obs_data_set_int(settings, "color1", color)
+        obs.obs_data_set_int(settings, "color2", color)
+
 # ------------------------------------------------------------
 
 def script_description():
@@ -162,9 +205,16 @@ def script_description():
     "stop worrying about whether you started your timer or not."
 
 def script_load(settings):
+    global use_podium_colors
+
     race_update_t = Thread(target=race_updater)
     race_update_t.daemon = True
     race_update_t.start()
+
+    use_podium_colors = obs.obs_data_get_bool(settings, "use_podium")
+
+def script_save(settings):
+    obs.obs_data_set_bool(settings, "use_podium", use_podium_colors)
 
 def script_unload():
     close_thread = True
@@ -174,12 +224,25 @@ def script_update(settings):
     global race
     global full_name
     global check_race_updates
+    global use_podium_colors
+    global first_color
+    global second_color
+    global third_color
+    global racing_color
+    global finished_color
 
     obs.timer_remove(update_text)
     
     source_name = obs.obs_data_get_string(settings, "source")
 
     race = obs.obs_data_get_string(settings, "race")
+
+    use_podium_colors = obs.obs_data_get_bool(settings, "use_podium")
+    first_color = obs.obs_data_get_int(settings, "first_color")
+    second_color = obs.obs_data_get_int(settings, "second_color")
+    third_color = obs.obs_data_get_int(settings, "third_color")
+    racing_color = obs.obs_data_get_int(settings, "racing_color")
+    finished_color = obs.obs_data_get_int(settings, "finished_color")
 
     if source_name != "":
         obs.timer_add(update_text, 100)
@@ -229,5 +292,18 @@ def script_properties():
 
     refresh = obs.obs_properties_add_button(props, "button", "Refresh", lambda *props: None)
     obs.obs_property_set_modified_callback(refresh, refresh_pressed)
+
+    p = obs.obs_properties_add_bool(props, "use_podium", "Use custom color for podium finishes?")
+    obs.obs_property_set_modified_callback(p, podium_toggled)
+
+    podium_group = obs.obs_properties_create()
+    obs.obs_properties_add_group(props, "podium_group", "Podium Colors", obs.OBS_GROUP_NORMAL, podium_group)
+    obs.obs_property_set_visible(obs.obs_properties_get(props, "podium_group"), use_podium_colors)
+
+    obs.obs_properties_add_color(podium_group, "racing_color", "Still racing:")
+    obs.obs_properties_add_color(podium_group, "first_color", "1st place:")
+    obs.obs_properties_add_color(podium_group, "second_color", "2nd place:")
+    obs.obs_properties_add_color(podium_group, "third_color", "3rd place:")
+    obs.obs_properties_add_color(podium_group, "finished_color", "After podium:")
 
     return props
